ef05aea7bbc6239a6a2f6198f138d8ceda42254c
Aviv Shrem <avivshrem@gmail.com>
2020-05-24 12:03:24 +0300

    changes in:
    -is diag : removed prints
    Aviv

--- Mathmetics Team/matrix.py
+++ Mathmetics Team/matrix.py
@@ -76,14 +76,6 @@ class Matrix():
         returns boolean : True- diagonalizable . else- False
         '''
 
-        #print(self.minPoly[:,0])
-        #print(self.charPoly[:,0])
-        print(numpy.array_equal(numpy.sort(self.minPoly[:,0]),numpy.sort(self.charPoly[:,0])))
-        #print(self.minPoly[:,1])
-        #a=np.ones_like(self.minPoly[:,1])
-
-        #print(a)
-        print(np.array_equal(self.minPoly[:,1],np.ones_like(self.minPoly[:,1])))
         is_diag= (numpy.array_equal(numpy.sort(self.minPoly[:,0]),numpy.sort(self.charPoly[:,0]))) and (np.array_equal(self.minPoly[:,1],np.ones_like(self.minPoly[:,1])))
         return is_diag
 
@@ -138,7 +130,7 @@ if __name__ == '__main__':
     '''
     Can do here some plays
     '''
-    arr =np.array([[1,0,0,0,0],[0,1,0,0,0],[0,0,2,0,1],[0,0,0,1,1],[0,0,0,0,2]])
+    arr =np.array([[1,0,0,0,0],[0,1,0,0,0],[0,0,2,0,0],[0,0,0,1,0],[0,0,0,0,2]])
     print(arr,"\n")
     mat = Matrix(arr)
     print(mat.getCharacteristicPolynomial(),"\n")
