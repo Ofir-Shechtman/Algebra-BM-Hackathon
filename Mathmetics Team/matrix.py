b3b4f58378feff45aa6d90b6494f7b1ceb12ff62
aviv9913 <aviv9913@gmail.com>
2020-05-31 19:17:06 +0300

    All merged

--- Mathmetics Team/matrix.py
+++ Mathmetics Team/matrix.py
@@ -1,6 +1,10 @@
 import numpy as np
 import numpy.linalg
 from numpy.linalg import matrix_rank
+import scipy.linalg as la
+import itertools
+import functools
+
 
 
 class Matrix():
@@ -11,6 +15,13 @@ class Matrix():
         self.matrix = matrix
         self.S = None
         self.N = None
+        self.matrix = matrix #the matrix: 2d np.ndarray
+        self.size = matrix.shape[0] # size of axis: int
+        self.eig_val,_ = np.linalg.eig(self.matrix) # eigen values with duplications : np.ndarray
+        self.charPoly = self.getCharacteristicPolynomial() #the characteristic Polynom in our presentation : 2d np.ndarray
+        self.minPoly = self.getMinimalPolynomial() #the minimal Polynom in our presentation : 2d np.ndarray
+        self.isDiagonal = self.isDiagonalizableMatrix() # boolean is diagonalizable matrix
+        self.eigan_vectors = self.getEigenvectors()#dictionary- eigenvals as keys,eigenvectors of each eigenval as ndarray *columns*
 
     def __call__(self, *args, **kwargs):
         '''
@@ -27,31 +38,60 @@ class Matrix():
 
     def getCharacteristicPolynomial(self):
         '''
-
         :return:
         '''
-        pass
+        unique_elements, counts_elements = np.unique(self.eig_val, return_counts=True)
+        charPoly = np.array([unique_elements,counts_elements])
+        return charPoly.transpose()
+
 
     def getMinimalPolynomial(self):
         '''
-
         :return:
         '''
-        pass
+        charP=self.charPoly
+        linear_factors = [(self.matrix - charP[i][0] * np.identity(self.size),charP[i][0]) for i in
+                          range(charP.shape[0])]  # list of all (A-lambda*I) factors in CharP
+        #print(linear_factors)
+        # loop from minimum num of linear factors in MinP to Size of matrix
+        for cur_num_of_lin_factors in range (self.charPoly.shape[0],self.size+1):
+            # list of combinations for cur_num_of_lin_factors componnents in poly
+            poly_lin_factors=itertools.combinations_with_replacement(linear_factors,cur_num_of_lin_factors)
+            poly_lin_factors=list(poly_lin_factors)
+            poly_results=[]
+            #calculate results of all polynoms (while removing the tuples with the eigen_values) and append into list
+            for cur_lin_factors_tups in poly_lin_factors:
+                cur_list=[cur_lin_factor[0] for cur_lin_factor in cur_lin_factors_tups]
+                poly_results.append(functools.reduce((lambda x, y: np.dot(x,y)),cur_list))
+
+            zero_index=[i for i in range(len(poly_results)) if numpy.count_nonzero(poly_results[i])==0]
+            # if there is a solution zero, get the result
+            if len(zero_index)>0:
+                min_lin_factors=[lin_factor[1] for lin_factor in poly_lin_factors[zero_index[0]]]
+                #np_arr=numpy.array(min_lin_factors)
+                unique_elements, counts_elements = np.unique(min_lin_factors, return_counts=True)
+                minPoly = np.array([unique_elements, counts_elements])
+                return minPoly.transpose()
+
+
+        return np.array() # error
+
 
     def isDiagonalizableMatrix(self):
         '''
-
-        :return:
+        returns boolean : True- diagonalizable . else- False
         '''
-        pass
+        #check if all in factors are included in minP and 1-time only
+        is_diag= (numpy.array_equal(numpy.sort(self.minPoly[:,0]),numpy.sort(self.charPoly[:,0]))) and (np.array_equal(self.minPoly[:,1],np.ones_like(self.minPoly[:,1])))
+        return is_diag
 
     def getEigenvectors(self):
         '''
-
-        :return:
+        :return: dict of eigen-value : eigen-vectors. {float, ndarray} ndarray=(mat_size,num_of_eigen_vectors)
         '''
-        pass
+        eigenlist = self.getCharacteristicPolynomial()[:,0]
+        dic = {eig: la.null_space(self.matrix - (eig * np.eye(self.matrix.shape[0]))) for eig in eigenlist}
+        return dic
 
     def getPmejardent(self):
         '''
